ac2fd565b6496f4846d9c2aaab311f5cad452c23
fietensen <fietensen@gmail.com>
2025-10-23 10:59:04 +0200

    checkstyle

--- src/main/java/com/ase/userservice/Application.java
+++ src/main/java/com/ase/userservice/Application.java
@@ -1,10 +1,10 @@
 package com.ase.userservice;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
 @EnableScheduling
