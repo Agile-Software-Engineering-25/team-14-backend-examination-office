89cd1617fb6361bbb0deef00901b92d00d0a5455
fietensen <fietensen@gmail.com>
2025-10-23 10:57:22 +0200

    add openapi

--- src/main/java/com/ase/userservice/Application.java
+++ src/main/java/com/ase/userservice/Application.java
@@ -1,11 +1,20 @@
 package com.ase.userservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Student API",
+        version = "1.0",
+        description = "API documentation for managing students"
+    )
+)
 public class Application {
 
   public static void main(String[] args) {
